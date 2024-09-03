4fd76be5359acd1c9a32064de3c5fa99092f192c
Eesa Hash <eesa6000@icloud.com>
2024-09-04 00:30:02 +1000

    Fixed UI elements, optimised clustering

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard.tsx";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
 
