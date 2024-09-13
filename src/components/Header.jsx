8bdec34eff9b5a1ca71b7af50cc02a3c1384e437
Eesa Hash <eesa6000@icloud.com>
2024-09-13 23:09:45 +1000

    changed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         onClick={() => {
           location.reload();
         }}
-        className="bg-standard border border-borderColor "
+        className="bg-black border border-borderColor "
         containerClassName="flex cursor-pointer bg-standard text-lg font-bold h-12 border-none mt-4 mb-4 desktop:mb-0 desktop:mt-12 ml-5 mr-5"
       >
         <div>Australian Waste Exports GIS Dashboard</div>
